1e0e64f59f60c5d81fde8c2f71ba5b40d402d9d3
KvietelaitisMartynas <martynaskvietelaitis@icloud.com>
2025-07-16 13:46:25 +0300

    hi

---
+++ i3/touchpad.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/bash
+
+xinput set-prop "FTE1200:00 0B05:0701 Touchpad" "libinput Scrolling Pixel Distance" 40
+xinput set-prop "FTE1200:00 0B05:0701 Touchpad" "libinput Natural Scrolling Enabled" 1
